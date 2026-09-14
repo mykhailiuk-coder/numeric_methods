a95383acda7b696e1071c17c3bf4d8b58e7b088d
mykhailiuk_vitalii <vitalii270406@gmail.com>
2026-09-14 22:33:34 +0300

    fix(lab1): analytical output for f1(x)

--- lab1/task1.py
+++ lab1/task1.py
@@ -223,8 +223,16 @@ roots_f2 = plot_graphical_separation(
 )
 
 print("Табличні інтервали f1(x):", table_method(f1, a=-10, b = 10))
-print("Аналітичний висновок f1(x)", analytical_method([1, -2, 0, 1, -1.5]))
+f1_analytical = analytical_method([1, -2, 0, 1, -1.5])
+print("Аналітичний висновок f1(x):\n", 
+      "Кількість коренів:", f1_analytical["total_roots"], "\n",
+      "Гарантовано дійсний корінь:", f1_analytical["has_real_guaranteed"], "\n",
+      "Максимальна кількість додатних коренів:", f1_analytical["pos_roots_max"], "\n",
+      "Максимальна кількість від'ємних коренів:", f1_analytical["neg_roots_max"], "\n",
+      "Необхідність наявності дійсності коренів:", f1_analytical["all_roots_real_necessary"], "\n",
+      "Кільце з усіма коренями:", f1_analytical["ring"]
+      )
 print("Знайдені корені для f1(x) графічно:", roots_f1)
-
+print("-----------------------------------")
 print("Табличні інтервали f2(x):", table_method(f2, a=0.5, b=4.0, h1=1.0))
 print("Знайдені корені для f2 графічно:", roots_f2)
